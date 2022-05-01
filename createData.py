687ba127757662d5b1f6050e0ad309dc926cba59
chodh0119 <62224270+chodh0119@users.noreply.github.com>
2022-05-01 14:38:53 -0400

    generate data

---
+++ createData.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+from anisotropicDiffusion import *
+from skimage import io
+import skimage
+import skimage.transform
+import argparse
+
+
+def main():
+
+	currentdir = os.path.dirname(os.path.realpath(__file__))
+	parentdir = os.path.dirname(currentdir)
+	rootrdir = os.path.dirname(os.path.dirname(parentdir))
+	data_dir = os.path.join(currentdir,'data')
+
+	parser = argparse.ArgumentParser(description='Diffusion')
+	parser.add_argument('-img_path',
+	                type=str, nargs=1, required=True)
+	parser.add_argument('-niter',
+	                type=int, nargs=1, required=True)
+	args = parser.parse_args()
+
+	img_path = args.img_path[0]
+	output_dir = data_dir
+	niter = args.niter[0]
+
+	img = io.imread(img_path)
+
+	print("-----Generating Data-----")
+
+	img = anisotropic_diffusion(output_dir, img, niter=niter, kappa=50, gamma=0.25).astype(img.dtype)
+
+	print("-----Finished-----")
+
+if __name__=="__main__":
+    main()
